5677a239ec5217972a8108d0dd79d343064ea578
Gus Varallo <gustavo.varallo@live.com>
2020-07-07 00:04:23 -0300

    Some SearchBar features

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
-function SearchBar() {
+function SearchBar({ searchYelp }) {
+  const [search, setSearch] = useState({
+    term: '',
+    location: '',
+    sortBy: 'best_match'
+  });
+
   const sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
   };
 
+  function getSortByClass(sortByOption) {
+    return search.sortBy === sortByOption ? 'active' : '';
+  }
+
+  function handleSortByChange(sortByOption) {
+    return setSearch({
+      sortBy: sortByOption
+    });
+  }
+
+  function handleTermChange(event) {
+    return setSearch({
+      term: event.target.value
+    });
+  }
+
+  function handleLocationChange(event) {
+    return setSearch({
+      location: event.target.value
+    });
+  }
+
+  function handleSearch(event) {
+    searchYelp(search.term, search.location, search.sortBy);
+    event.preventDefault();
+  }
+
   function renderSortByOptions() {
     return Object.keys(sortByOptions).map(el => {
       let sortByOptionValue = sortByOptions[el];
-      return <li key={sortByOptionValue}>{el}</li>;
+      return <li 
+              onClick={() => handleSortByChange(sortByOptionValue)} 
+              className={getSortByClass(sortByOptionValue)} 
+              key={sortByOptionValue}>{el}</li>;
     });
   }
 
@@ -23,11 +59,11 @@ function SearchBar() {
         </ul>
       </div>
       <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
+        <input onChange={handleTermChange} placeholder="Search Businesses" />
+        <input onChange={handleLocationChange} placeholder="Where?" />
       </div>
       <div className="SearchBar-submit">
-        <a>Let's Go</a>
+        <button onClick={handleSearch}>Let's Go</button>
       </div>
     </div>
   );
